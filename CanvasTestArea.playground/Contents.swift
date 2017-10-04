1bf8af807237addaaf82ead6700ee13f5635f50c
Lu, Alisa <alu@lcs-mba3124.lcs.on.ca>
2017-10-04 12:11:19 -0400

    ears. eyes and face

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -29,10 +29,29 @@ canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
 // Show where the origin is
 canvas.drawAxes()
 
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+// Draw the left ear
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 100, centreY: 450, width:80, height: 70)
 
+// Draw the right ear
+canvas.drawEllipse(centreX: 250, centreY: 450, width:80, height: 70)
 
-// This code is necessary to see the result in the Assistant Editor at right
+//Draw the face
+canvas.drawShapesWithBorders = false
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 170, centreY: 380, width:200, height: 180)
+
+//draw the left eye
+canvas.drawShapesWithBorders = true
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 120, centreY: 410, width: 50, height: 40)
+
+//draw the right eye
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 220, centreY: 410, width: 50, height: 40)
+
+//draw the left eye white
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 130, centreY: <#T##Int#>, width: <#T##Int#>, height: <#T##Int#>)
+// Draw the face
 PlaygroundPage.current.liveView = canvas.imageView
