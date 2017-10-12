2a8be4cbb8e504f384f106390e07652d3030075b
Lu, Alisa <alu@lcs-mba3124.lcs.on.ca>
2017-10-12 19:44:50 -0400

    Alisa's panda image

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -31,13 +31,13 @@ canvas.drawAxes()
 
 // Draw the left ear
 canvas.fillColor = Color.black
-canvas.drawEllipse(centreX: 100, centreY: 450, width:80, height: 70)
+canvas.drawEllipse(centreX: 100, centreY: 450, width:60, height: 50)
 
 // Draw the right ear
-canvas.drawEllipse(centreX: 250, centreY: 450, width:80, height: 70)
+canvas.drawEllipse(centreX: 250, centreY: 450, width:60, height: 50)
 
 //Draw the face
-canvas.drawShapesWithBorders = false
+canvas.drawShapesWithBorders = true
 canvas.fillColor = Color.white
 canvas.drawEllipse(centreX: 170, centreY: 380, width:200, height: 180)
 
@@ -52,6 +52,30 @@ canvas.drawEllipse(centreX: 220, centreY: 410, width: 50, height: 40)
 
 //draw the left eye white
 canvas.fillColor = Color.white
-canvas.drawEllipse(centreX: 130, centreY: <#T##Int#>, width: <#T##Int#>, height: <#T##Int#>)
-// Draw the face
+canvas.drawEllipse(centreX: 130, centreY:411, width: 10, height: 10)
+
+//draw the right eye white
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 210, centreY: 411, width: 10, height: 10)
+
+//draw the nose
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 170, centreY: 380, width: 25, height: 20)
+
+//draw the mouth up
+canvas.fillColor = Color.black
+canvas.drawLine(fromX: 170, fromY: 360, toX: 170, toY: 380)
+
+//draw the mouth up left
+canvas.fillColor = Color.black
+canvas.drawLine(fromX: 160, fromY: 350, toX: 170, toY: 360)
+
+//draw the mouth up right
+canvas.fillColor = Color.black
+canvas.drawLine(fromX: 180, fromY: 350, toX: 170, toY: 360)
+
+
+
+
+
 PlaygroundPage.current.liveView = canvas.imageView
